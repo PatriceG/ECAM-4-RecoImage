a2dbb0d55a1bb310900fae528f7202cfc7fdda5e
Patrice GODARD <patrice.godard@orange.com>
2023-09-15 14:28:40 +0200

    minor fixes in comments

--- src/fr/ecam/recoimage/RecoImage.java
+++ src/fr/ecam/recoimage/RecoImage.java
@@ -13,7 +13,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
- * Classe illustrant l'utilisation du client HTTP int√©gr√© √† Java 11 et de
+ * Classe illustrant l'utilisation du client HTTP intÈgrÈ ‡†Java 11+ et de
  * clarifai.com pour faire de la classification d'image
  *
  * @author pat
@@ -22,24 +22,24 @@ public class RecoImage {
 
 	/**
 	 * 
-	 * @param url- URL de l'image √† classifier
-	 * @return flux JSON retourn√© par clarifai
+	 * @param url - URL de l'image ‡ classifier
+	 * @return flux JSON retournÈ par clarifai
 	 */
 	public String classifyImage(String url) throws Exception {
 		String res = null;
 
 		try {
-			String MODEL_ID = "aaa03c23b3724a16a56b629203edc62c"; // mod√®le de classification d'images "g√©n√©ral"
+			String MODEL_ID = "aaa03c23b3724a16a56b629203edc62c"; // modËle de classification d'images "gÈnÈral"
 			String CLARIFAI_API_KEY = "3f11c47ba665437692273b01e4e97af5";
-			// URL du service √† appeler
+			// URL du service ‡† appeler
 			String service_url = String.format("https://api.clarifai.com/v2/models/%s/outputs", MODEL_ID);
 
 			// construction manuelle de JSON, envisageable pour un petit flux, sinon on
-			// passe plut√¥t par l'API de manipulation de JSON
+			// passe plut‡¥t par l'API de manipulation de JSON
 			String payload = "{\"inputs\": [{\"data\": {\"image\": {\"url\": \"" + url + "\"}}}]}";
 
 			// TODO PARTIE 2 du TP: utiliser le Jackson TreeModel pour construire le flux
-			// payload au lieu de passer par une chaine de caract√®res
+			// payload au lieu de passer par une chaine de caractËres
 
 			// Note: ne fonctionne pas pour l'appel du service, mais on peut l'activer en
 			// phase de debug
@@ -47,12 +47,12 @@ public class RecoImage {
 			// TreeModel
 			// mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-			// r√©cup√®re la payload JSON sous forme d'une chaine de caract√®res
+			// rÈcupËre la payload JSON sous forme d'une chaine de caractËres
 			// String payload = mapper.writeValueAsString(root);
 
-			// construit une requ√™te via l'API builder, avec tous les en-t√™tes et la
+			// construit une requËte via l'API builder, avec tous les en-tÍtes et la
 			// payload:
-			// noter le mot-cl√© var apparu avec Java11 qui permet de ne pas utiliser le type
+			// noter le mot-clÈ var apparu avec Java11 qui permet de ne pas utiliser le type
 			// explicite de la variable
 			var request = HttpRequest.newBuilder().uri(URI.create(service_url))
 					.header("Content-Type", "application/json")
@@ -62,10 +62,10 @@ public class RecoImage {
 			System.out.println("Envoi requete vers: " + service_url);
 			System.out.println("avec la payload: " + payload);
 
-			// Ex√©cution requete
+			// ExÈcution requete
 			var client = HttpClient.newHttpClient();
 
-			// Lecture r√©ponse au format String
+			// Lecture rÈponse au format String
 			var response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 			System.out.println("----------------------------------------");
@@ -78,26 +78,26 @@ public class RecoImage {
 	}
 
 	/**
-	 * Affiche un tableau des concepts et de leur probabilit√©
+	 * Affiche un tableau des concepts et de leur probabilitÈ
 	 * 
-	 * @param data flux json retourn√© par clarifai
+	 * @param data flux json retournÈ par clarifai
 	 */
 	public void displayConcepts(String data) {
-		System.out.println("Concepts d√©tect√©s:");
-		System.out.println("flux JSON brut retourn√©: " + data);
+		System.out.println("Concepts dÈtectÈs:");
+		System.out.println("flux JSON brut retournÈ: " + data);
 
 		ObjectMapper mapper = new ObjectMapper();
 		try {
 			JsonNode root = mapper.readTree(data);
 
-			// TODO: utiliser Jackson TreeModel pour r√©cup√©rer et afficher les concepts
-			// retourn√©s pour l'image sp√©cifi√©e ainsi que
-			// le score de probabilit√© associ√©
+			// TODO: utiliser Jackson TreeModel pour rÈcupÈrer et afficher les concepts
+			// retournÈs pour l'image spÈcifiÈe ainsi que
+			// le score de probabilitÈ associÈ
 			// afficher ces deux infos sur la console
 
-			// attention le flux de retour contient des infos relatives √† des images
+			// attention le flux de retour contient des infos relatives ‡† des images
 			// d'exemple.
-			// il faut retourner les concepts associ√©s √† l'image provenant du serveur
+			// il faut retourner les concepts associÈs ‡† l'image provenant du serveur
 			// vps284011.ovh.net
 			// exemple:
 			/*
@@ -107,11 +107,11 @@ public class RecoImage {
 			 * "main" }....
 			 */
 
-			// TODO: compl√©ter le code
+			// TODO: complÈter le code
 			// note: vous pouvez utiliser ce site pour formatter un flux json pour une
-			// facilit√© de lecture: https://jsonformatter.org/
-			// copier/coller y le flux JSON brut affich√© sur la console
-			// pour vous aider √† naviguer dans l'arbre JSON avec l'API Jackson
+			// facilitÈ de lecture: https://jsonformatter.org/
+			// copier/coller y le flux JSON brut affichÈ sur la console
+			// pour vous aider ‡† naviguer dans l'arbre JSON avec l'API Jackson
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -120,12 +120,12 @@ public class RecoImage {
 	}
 
 	/**
-	 * Affiche l'horaire de prochain passage du bus de la ligne sp√©cifi√©e √† l'arr√™t
-	 * sp√©cifi√©
+	 * Affiche l'horaire de prochain passage du bus de la ligne spÈcifiÈe ‡† l'arr‡™t
+	 * spÈcifiÈ
 	 * 
 	 * @param nomCourtLigne - nom court de la ligne: 57 pour celle desservant Ker
 	 *                      Lann
-	 * @param idArret       - identifnant de l'arr√™t: 2311 pour celui devant l'ECAM
+	 * @param idArret       - identifnant de l'arr‡™t: 2311 pour celui devant l'ECAM
 	 */
 	public void afficheHoraireBus(String nomCourtLigne, int idArret) {
 
@@ -134,37 +134,37 @@ public class RecoImage {
 				nomCourtLigne, idArret);
 		System.out.println("Appel de: " + service_url);
 
-		// construit une requ√™te via l'API builder
-		// noter le mot-cl√© var apparu avec Java11 qui permet de ne pas utiliser le type
+		// construit une requ‡™te via l'API builder
+		// noter le mot-clÈ var apparu avec Java11 qui permet de ne pas utiliser le type
 		// explicite de la variable
 		var request = HttpRequest.newBuilder().uri(URI.create(service_url)).GET().build();
 
-		// Ex√©cution requete
+		// ExÈcution requete
 		var client = HttpClient.newHttpClient();
 
-		// Lecture r√©ponse au format String
+		// Lecture rÈponse au format String
 		try {
 			var response = client.send(request, HttpResponse.BodyHandlers.ofString());
-			System.out.println("R√©ponse Brute:" + response.body());
+			System.out.println("RÈponse Brute:" + response.body());
 
 			// TODO: Parser le flux avec le Jackson TreeModel: instancier un ObjectMapper et
 			// parser via mapper.readTree(responde.body())
 
 	
 			//TODO:
-			// prendre le dernier √©l√©ment de records[]
-			// Attention records[] peut √™tre vide si aucun bus n'est pr√©vu √† cet arr√™t
-			// d√©tecter ce cas (via la m√©thode isEmpty(null) de JsonNode et afficher un
+			// prendre le dernier ÈlÈment de records[]
+			// Attention records[] peut ‡™tre vide si aucun bus n'est prÈvu ‡† cet arr‡™t
+			// dÈtecter ce cas (via la mÈthode isEmpty(null) de JsonNode et afficher un
 			// message dans ce cas)
 
-			// il n'y a pas de moyen simple d'aller au dernier √©l√©ment du tableau de records
-			// => on r√©cup√®re un Iterator<JsonNode> sur le tableau via
+			// il n'y a pas de moyen simple d'aller au dernier ÈlÈment du tableau de records
+			// => on rÈcup‡®re un Iterator<JsonNode> sur le tableau via
 			// recordsNode.iterator() et on le parcourt
-			// en entier en stockant le JsonNode retourn√© par la m√©thode next() de
-			// l'iterator dans une variable locale, utilis√©e par la suite
+			// en entier en stockant le JsonNode retournÈ par la mÈthode next() de
+			// l'iterator dans une variable locale, utilisÈe par la suite
 
-			// en sortie de la boucle while on on a l'heure de passage estim√©e dans l'attribut arriveetheorique de l'attribut fields du dernier record
-			// aide: on aura un truc du genre lastRecord.path("fields").path("arriveetheorique").asText() pour r√©cup√©rer l'heure de passage
+			// en sortie de la boucle while on on a l'heure de passage estimÈe dans l'attribut arriveetheorique de l'attribut fields du dernier record
+			// aide: on aura un truc du genre lastRecord.path("fields").path("arriveetheorique").asText() pour rÈcupÈrer l'heure de passage
 			
 	
 		} catch (IOException | InterruptedException e) {
@@ -180,13 +180,13 @@ public class RecoImage {
 	public static void main(String[] args) {
 		RecoImage ri = new RecoImage();
 
-		// URL de l'image √† classifier, √† adapter √† votre nom de VM.
+		// URL de l'image ‡† classifier, ‡† adapter ‡† votre nom de VM.
 		String urlImage = "http://vps284011.ovh.net/ecam/image-1.jpg";
 
-		// commencez par ex√©cuter le code en changeant d'image dans le code (10 images
+		// commencez par exÈcuter le code en changeant d'image dans le code (10 images
 		// disponibles).
-		// ensuite, faites en sorte de lire le nom de l'image √† utiliser depuis la
-		// console (voir le cours sur les Entr√©es/Sorties)
+		// ensuite, faites en sorte de lire le nom de l'image ‡† utiliser depuis la
+		// console (voir le cours sur les EntrÈes/Sorties)
 
 		String res;
 		try {
